a96b22a7e479a4eb318a212e2def12c1774dfc46
Juan Snow <95362321+juansnow01@users.noreply.github.com>
2022-05-24 19:13:18 +0100

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,54 @@
+# This function adds two values
+def add(x, y):
+    return x + y
+
+# This function subtracts two values
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two values
+def multiply(x, y):
+    return x * y
+
+
+# This function divides two values
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # take input from the user
+    choice = input("Choose an action: ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first value: "))
+        num2 = float(input("Enter second value: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+            break
+
+    else:
+        print("Invalid Input")
+
